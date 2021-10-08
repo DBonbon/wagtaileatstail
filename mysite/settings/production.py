1373cb1486b7fa37fcfd78f973796c2e2b097854
Yariv Friedman <yrfriedman@gmail.com>
2021-10-08 16:59:00 +0200

    run with psql 12 only

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '167.172.109.147']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '167.172.109.147']
 SECRET_KEY = 'django-insecure--jxsf@pi^*z=hvz8+=v3+f0-vqw+j^6)#^(*6$%jlfy#b&%u!6'
 
 try:
